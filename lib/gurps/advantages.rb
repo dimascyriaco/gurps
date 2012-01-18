f2cc5860712ef01c781f8efe4e4fc17082e2ba75
dimascyriaco <dimascyriaco@gmail.com>
2012-01-17 23:18:40 -0200

    Advantage yml example

--- lib/gurps/advantages.rb
+++ lib/gurps/advantages.rb
@@ -1,7 +1,7 @@
 module Gurps
   class Advantage
     def self.available
-      $advantages ||= {:skinny => {cost: -5}}
+      $advantages ||= {skinny: {cost: -5, modifiers: {weight: {level: '*2/3'}, disguise: {level: '-2'}, shadowing: {level: '-2'}, health: {upper_limit: '14'}}, secondary_effects: {strength: {level: '-2', situation: 'resist knockback'}}}}
       $advantages.keys
     end
 
