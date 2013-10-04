42ea26331f63bde2920c7e6860fbca32c3bc72b2
Dimas Cyriaco <dimascyriaco@gmail.com>
2013-10-04 01:58:16 -0300

    Modifying the advantage yaml format to include the advantage type and renaming 'level' to 'value'

--- spec/gurps/advantage_spec.rb
+++ spec/gurps/advantage_spec.rb
@@ -5,7 +5,7 @@ describe Gurps::Advantage do
     {fat: {
         cost: '-5',
         modifiers: [
-            {target: 'weight', level: '*2/3'}
+            {target: 'weight', type: 'multiplier', value: '2/3'}
         ]
     }}
   end
@@ -14,48 +14,48 @@ describe Gurps::Advantage do
     Gurps::Advantage.register(:fat, advantage[:fat])
   end
 
-  it "should list available advantages" do
+  it 'should list available advantages' do
     Gurps::Advantage.available.should include(:fat)
   end
 
-  it "should be accessible via method and via hash" do
+  it 'should be accessible via method and via hash' do
     Gurps::Advantage.fat
     Gurps::Advantage[:fat].should_not be_nil
   end
 
-  context "buying an advantage" do
-    it "should discount the cost from the character available poinnts" do
+  context 'buying an advantage' do
+    it 'should discount the cost from the character available points' do
       char = Gurps::Character.new
       lambda do
         char.buy(:fat)
       end.should change(char, :points_to_spend).by(-5)
     end
 
-    it "should discount the cost from the character available poinnts" do
+    it 'should discount the cost from the character available points' do
       char = Gurps::Character.new
-      lambda do
-        char.buy(:fat)
-      end.should change(char, :weight).by(-5)
+      expect(char.weight).to eq(150)
+      char.buy(:fat)
+      expect(char.weight).to eq(100)
     end
   end
 
-  context "Registering a new advantage" do
-
-    it "should register a new advantage" do
+  context 'Registering a new advantage' do
+    it 'should register a new advantage' do
       Gurps::Advantage.fat
     end
 
-    it "should set the cost" do
+    it 'should set the cost' do
       Gurps::Advantage.fat.cost.should == '-5'
     end
 
-    it "should set the modifiers" do
+    it 'should set the modifiers' do
       Gurps::Advantage.fat.modifiers.should_not be_nil
     end
 
-    it "should allow access to the modifications properties" do
+    it 'should allow access to the modifications properties' do
       Gurps::Advantage.fat.modifiers.first.target.should == 'weight'
-      Gurps::Advantage.fat.modifiers.first.level.should == '*2/3'
+      Gurps::Advantage.fat.modifiers.first.type.should == 'multiplier'
+      Gurps::Advantage.fat.modifiers.first.value.should == '2/3'
     end
   end
 end
